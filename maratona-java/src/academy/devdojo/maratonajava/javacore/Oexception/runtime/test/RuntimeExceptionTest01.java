98e66e6252e267e611ce33e6e03671c93377917b
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-12-15 22:26:33 -0300

    Excecoes pt1

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+//        NullPointerException
+
+//        Object obj = null;
+//        System.out.println(obj.toString());
+
+//        ArrayIndexOutOfBoundsException
+
+        int[] nums = {1,2};
+        System.out.println(nums[2]);
+    }
+}
