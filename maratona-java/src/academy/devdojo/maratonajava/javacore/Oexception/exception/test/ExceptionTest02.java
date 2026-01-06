5f6c88c24c3a6024efeea767f646be22e3433e5a
Ygor Gabriel Silva <silvaygor16@gmail.com>
2026-01-06 20:28:27 -0300

    Aprendendo a usar throws

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException{
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() throws IOException{
+        File file = new File("arquivo/arquivo.txt");
+
+        try{
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado: " + isCriado);
+        }catch (IOException e){
+            System.err.println("Erro: " + e.getMessage());
+            throw e;
+        }
+
+    }
+}
