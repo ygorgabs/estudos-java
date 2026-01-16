d1f27d7089288539d5d38082163eaddd1a5da8d7
Ygor Gabriel Silva <silvaygor16@gmail.com>
2026-01-15 22:17:02 -0300

    Wrapper pt 1

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Pwrapper/WrapperTest01.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+
+        // Tipos primitivos
+
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 1L;
+        float floatP = 1F;
+        double doubleP = 1D;
+        char charP ='W';
+        boolean booleanP = false;
+
+
+        //Wrapper Classes
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 1L;
+        Float floatW = 1F;
+        Double doubleW = 1D;
+        Character charW ='W';
+        Boolean booleanW = false;
+
+
+    }
+}
