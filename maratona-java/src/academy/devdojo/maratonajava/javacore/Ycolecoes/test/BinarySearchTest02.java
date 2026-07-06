194a765d4e03346bc0dd5c6c72ad78a07c789ad2
Ygor Gabriel Silva <silvaygor16@gmail.com>
2026-07-06 18:06:33 -0300

    binarySearch metodo

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/BinarySearchTest02.java
@@ -4,18 +4,13 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-class MangaByName implements Comparator<Manga>{
-
-    @Override
-    public int compare(Manga manga1, Manga manga2) {
-        return manga1.getNome().compareTo(manga2.getNome());
-    }
-}
-public class MangaSortTest01 {
+public class BinarySearchTest02 {
     public static void main(String[] args) {
+
+        MangaByName comparator = new MangaByName();
+
         List<Manga> mangas = new ArrayList<>();
         mangas.add(new Manga(5,"Jujustsu Kaisen",10.21));
         mangas.add(new Manga(3,"Dragon Ball",29.98));
@@ -23,20 +18,14 @@ public class MangaSortTest01 {
         mangas.add(new Manga(2, "Naruto", 98.1));
         mangas.add(new Manga(4, "Kimetsu no Yaiba", 30.2));
 
+//        Collections.sort(mangas);
+        mangas.sort(comparator);
+        Manga mangaToSearch = new Manga(3,"Dragon Ball",29.98);
+
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
 
-        System.out.println("-------------------");
-        Collections.sort(mangas);
-        for (Manga manga : mangas) {
-            System.out.println(manga);
-        }
-
-        System.out.println("-------------------");
-        Collections.sort(mangas, new MangaByName());
-        for (Manga manga : mangas) {
-            System.out.println(manga);
-        }
+        System.out.println(Collections.binarySearch(mangas, mangaToSearch, comparator));
     }
 }
