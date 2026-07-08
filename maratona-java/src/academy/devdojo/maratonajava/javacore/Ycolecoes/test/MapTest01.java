df792683967bdf78e7c15cf456925b6ac5971671
Ygor Gabriel Silva <silvaygor16@gmail.com>
2026-07-08 19:09:57 -0300

    Map e HashMap pt1

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapTest01 {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("teklado", "teclado");
+        map.put("mauze", "mouse");
+        map.put("vc", "você");
+        map.put("vc2", "você");
+        System.out.println(map);
+
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+        System.out.println("--------------------------");
+        for (String value : map.values()) {
+            System.out.println(value);
+        }
+        System.out.println("--------------------------");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
