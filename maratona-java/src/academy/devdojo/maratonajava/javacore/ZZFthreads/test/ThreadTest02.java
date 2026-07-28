be62c816b15e0630847cb012e6976c885417aea7
Ygor Gabriel Silva <silvaygor16@gmail.com>
2026-07-28 17:25:26 -0300

    Threads pt3

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest02.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
+
+class ThreadExampleRunnable2 implements Runnable {
+    private String c;
+
+    public ThreadExampleRunnable2(String c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+            Thread.yield();
+        }
+    }
+}
+
+public class ThreadTest02 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ThreadExampleRunnable2("KA"));
+        Thread t2 = new Thread(new ThreadExampleRunnable2("ME"));
+        t1.start();
+        t1.join();
+        t2.start();
+    }
+}
